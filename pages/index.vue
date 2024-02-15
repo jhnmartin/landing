a07aae45bb535fa38dae0833f322ac6ede6614b9
John Martin <74799962+jhnmartin@users.noreply.github.com>
2024-02-14 21:36:36 -0600

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -70,23 +70,6 @@ const cardTitle = ref({
         class="w-full h-auto rounded-lg border-2 border-primary-100 dark:border-primary-400"
       />
     </ULandingHero>
-
-    <ULandingSection
-      :title="page.features.title"
-      :description="page.features.description"
-      :headline="page.features.headline"
-    >
-      <UPageGrid
-        id="features"
-        class="scroll-mt-[calc(var(--header-height)+140px+128px+96px)]"
-      >
-        <ULandingCard
-          v-for="(item, index) in page.features.items"
-          :key="index"
-          v-bind="item"
-        />
-      </UPageGrid>
-    </ULandingSection>
     <ULandingSection
       id="pricing"
       :title="page.pricing.title"
@@ -147,6 +130,22 @@ const cardTitle = ref({
           <EbTicketModal eventbrite-id="795737350837" />
         </ULandingCard>
       </ULandingGrid>
+    </ULandingSection>
+    <ULandingSection
+      :title="page.features.title"
+      :description="page.features.description"
+      :headline="page.features.headline"
+    >
+      <UPageGrid
+        id="features"
+        class="scroll-mt-[calc(var(--header-height)+140px+128px+96px)]"
+      >
+        <ULandingCard
+          v-for="(item, index) in page.features.items"
+          :key="index"
+          v-bind="item"
+        />
+      </UPageGrid>
     </ULandingSection>
 
     <!--
