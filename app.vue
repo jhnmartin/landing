fe9b0fc78824cedaf9496fa7026ddb6b92152094
John Martin <74799962+jhnmartin@users.noreply.github.com>
2024-02-14 19:51:31 -0600

    update seo

--- app.vue
+++ app.vue
@@ -1,20 +1,16 @@
 <script setup lang="ts">
 useHead({
-  meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-  ],
-  link: [
-    { rel: 'icon', href: '/favicon.ico' }
-  ],
+  meta: [{ name: "viewport", content: "width=device-width, initial-scale=1" }],
+  link: [{ rel: "icon", href: "/favicon.ico" }],
   htmlAttrs: {
-    lang: 'en'
-  }
-})
+    lang: "en",
+  },
+});
 
 useSeoMeta({
-  ogSiteName: 'Nuxt UI Pro - Landing template',
-  twitterCard: 'summary_large_image'
-})
+  ogSiteName: "Im from Cleveland X Bar Crawl LIVE!",
+  twitterCard: "summary_large_image",
+});
 </script>
 
 <template>
